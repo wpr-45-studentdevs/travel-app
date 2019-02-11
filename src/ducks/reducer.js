db8354cf38bc3c9fa955e10c79097678d9b416d8
kspayne93 <kyle@investavante.com>
2019-02-11 15:00:01 -0700

    added redux store and reducer files

---
+++ src/ducks/reducer.js
@@ -0,0 +1,24 @@
+const initialState = {
+   user: {},
+}
+
+//ACTION TYPES
+const GET_USER_DATA = 'GET_USER_DATA'
+
+//ACTION CREATORS
+export function getUserData(userData) {
+   return {
+      type: GET_USER_DATA,
+      payload: userData
+   }
+}
+
+//REDUCER
+export default function reducer(state = initialState, action) {
+   switch(action.type) {
+      case GET_USER_DATA: 
+         return {...state, user: action.payload}
+      default:
+         return state
+   }
+}

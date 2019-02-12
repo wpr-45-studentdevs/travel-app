7909ae20ee007d4f3494a0368227c985426344c1
finmorrison <fin.morrison@gmail.com>
2019-02-12 09:16:59 -0700

    header started

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
+import Header from '../Header/Header'
 
 export default class Dashboard extends Component {
    render() {
       return (
          <div>
+            <Header/>
             <h1>Dashboard</h1>
          </div>
       )

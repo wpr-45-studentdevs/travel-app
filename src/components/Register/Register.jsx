938f35462768cabd69418c3683e77f2c7423e7d1
Lily Alvarado <emalvarado90@Gmail.com>
2019-02-11 18:24:12 -0700

    login, register and mytrips skeletons

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,11 +1,38 @@
 import React, { Component } from 'react'
+import './Register.scss'
 
 export default class Register extends Component {
+  state = {
+    email: '',
+    password: '',
+    confirm: ''
+  }
    render() {
       return (
-         <div>
-            <h1>Register</h1>
-         </div>
+        <div className='register'>
+        <div className='registerBox'>
+        <h1>Register</h1>
+        <div>
+        Email:
+        <br/>
+        <input placeholder='email' type="text"/>
+        </div>
+        <br/>
+        <div>
+        Password:
+        <br/>
+        <input placeholder='password' type="password"/>
+        </div>
+        <br/>
+        <div>
+        Confirm Password:
+        <br/>
+        <input type="password"/>
+        </div>
+        <br/>
+        <button>Register</button>
+        </div>
+     </div>
       )
    }
 }

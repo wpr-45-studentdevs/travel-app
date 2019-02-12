7dea332e6218f691629a284cbbec23e9ec6c643e
Samuel <samuelurquhart96@gmail.com>
2019-02-12 09:31:49 -0700

    added landing header to auth pages

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './Register.scss'
+import LandingNav from '../LandingNav/LandingNav';
 
 export default class Register extends Component {
   state = {
@@ -7,32 +8,35 @@ export default class Register extends Component {
     password: '',
     confirm: ''
   }
-   render() {
-      return (
+  render() {
+    return (
+      <>
+        <LandingNav auth />
         <div className='register'>
-        <div className='registerBox'>
-        <h1>Register</h1>
-        <div>
-        Email:
-        <br/>
-        <input placeholder='email' type="text"/>
+          <div className='registerBox'>
+            <h1>Register</h1>
+            <div>
+              Email:
+        <br />
+              <input placeholder='email' type="text" />
+            </div>
+            <br />
+            <div>
+              Password:
+        <br />
+              <input placeholder='password' type="password" />
+            </div>
+            <br />
+            <div>
+              Confirm Password:
+        <br />
+              <input type="password" />
+            </div>
+            <br />
+            <button>Register</button>
+          </div>
         </div>
-        <br/>
-        <div>
-        Password:
-        <br/>
-        <input placeholder='password' type="password"/>
-        </div>
-        <br/>
-        <div>
-        Confirm Password:
-        <br/>
-        <input type="password"/>
-        </div>
-        <br/>
-        <button>Register</button>
-        </div>
-     </div>
-      )
-   }
+      </>
+    )
+  }
 }

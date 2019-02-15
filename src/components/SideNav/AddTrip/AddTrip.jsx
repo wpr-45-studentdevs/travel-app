8a95440590a8218394c83cb85d2799928936ffd8
finmorrison <fin.morrison@gmail.com>
2019-02-15 16:52:22 -0700

    working on add a trip. not finished

---
+++ src/components/SideNav/AddTrip/AddTrip.jsx
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+import "./AddTrip.scss";
+import Axios from "axios";
+
+export default class AddTrip extends Component {
+  state = {
+    toggleModal: false,
+    tripName: "",
+    date: "",
+    completed: false,
+    public: false,
+    tripLength: 0,
+    activities: [],
+    locations: []
+  };
+
+  handleChange = (prop, val) => {
+    this.setState({ [prop]: val });
+    console.log(this.state);
+  };
+
+ 
+  addTripDetails = async () => {
+    await Axios.post("/api/add-trip", {
+      tripName: this.state.tripName,
+      date: this.state.date,
+      completed: this.state.completed,
+      public: this.state.public,
+      tripLength: this.state.tripLength
+    }).then(response=>{console.log(response)})
+  };
+
+  render() {
+    return (
+      <>
+        <li
+          className="add-trip-button"
+          onClick={() => this.setState({ toggleModal: true })}
+        >
+          Add Trip
+        </li>
+        {/* MODAL */}
+        {this.state.toggleModal === true ? (
+          <div className="modalFullPage">
+            <div className="modalAddTrip">
+              <input
+                className="addTripInputs"
+                onChange={e => this.handleChange("tripName", e.target.value)}
+                type='text'
+                placeholder="Trip Name"
+              />
+              <input
+                className="addTripInputs"
+                onChange={e => this.handleChange("date", e.target.value)}
+                type='text'
+                placeholder="Trip Date"
+              />
+              <input
+                className="addTripInputs"
+                onChange={e => this.handleChange("tripLength", e.target.value)}
+                type = 'number'
+                placeholder="Trip Length"
+              />
+              <div className="checkboxes">
+                <span>Trip Completed?</span>
+                <input type="checkbox" placeholder="trip name" onClick={()=>this.setState({completed: !this.state.completed})}/>
+              </div>
+              <div className="checkboxes">
+                <span>Trip Public?</span>
+                <input type="checkbox" placeholder="trip name" onClick={()=>this.setState({public: !this.state.public})}/>
+              </div>
+              <button onClick={()=> this.addTripDetails()}>Save</button>
+              <button onClick={() => this.setState({ toggleModal: false })}>
+                Close
+              </button>
+            </div>
+          </div>
+        ) : null}
+      </>
+    );
+  }
+}

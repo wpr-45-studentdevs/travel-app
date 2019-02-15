45a2a92193cb8b3c417ede9b6520079f1c7a826d
kspayne93 <kyle@investavante.com>
2019-02-15 09:27:03 -0700

    styled

--- src/components/LandingNav/LandingNav.jsx
+++ src/components/LandingNav/LandingNav.jsx
@@ -7,19 +7,14 @@ export default function LandingNav(props) {
       <div className='landing-nav-container'>
          <nav className='landing-nav'>
             <h1 className='landing-nav-title'>Kanoo</h1>
-            {props.auth ?
-               <> </>
-               :
-               <>
-                  <div>
-                     <Link to='/login' className='landing-nav-buttons'>
-                        Login
-                     </Link>
-                     <Link to='/register' className='landing-nav-buttons'>
-                        Register
-                     </Link>
-                  </div>
-               </>}
+               <div>
+                  <Link to='/login' className='landing-nav-login-button'>
+                     Login
+                  </Link>
+                  <Link to='/register' className='landing-nav-register-button'>
+                     Register
+                  </Link>
+               </div>
          </nav>
       </div>
    )

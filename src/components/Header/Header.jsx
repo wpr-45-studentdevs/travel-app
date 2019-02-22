a949ce2a773ce4b106e1e26901254ccb4de26c88
kspayne93 <kyle@investavante.com>
2019-02-21 20:19:41 -0700

    removed underline in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import "./Header.scss";
 import UserMenu from '../Header/UserButton/UserButton';
 import Logo from '../../images/kanoo_logo3.svg'
 import axios from 'axios'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 import { getUserData } from '../../ducks/reducer'
 import UserImgPlaceholder from '../../images/userImgPlaceholder.jpg'
 
@@ -18,13 +18,13 @@ class Header extends Component {
     const res = await axios.get('/auth/userData')
     if (res.data) {
       await this.props.getUserData(res.data)
-     await this.getprofileImg()
+      await this.getprofileImg()
     }
   }
 
-  componentDidUpdate (prevProps) {
-    if(prevProps.img !== this.props.img) {
-      if(this.props.img){
+  componentDidUpdate(prevProps) {
+    if (prevProps.img !== this.props.img) {
+      if (this.props.img) {
         this.getprofileImg()
       } else {
         this.setState({
@@ -35,9 +35,9 @@ class Header extends Component {
   }
 
   getprofileImg = async () => {
-    const {user_id} = this.props.user
+    const { user_id } = this.props.user
     let res = await axios.get(`/api/userInfo/${user_id}`)
-    if(res.data[0].profile_pic) {
+    if (res.data[0].profile_pic) {
       await this.setState({
         profileImg: res.data[0].profile_pic
       })
@@ -45,7 +45,7 @@ class Header extends Component {
   }
 
   render() {
-    const {profileImg} = this.state
+    const { profileImg } = this.state
     return (
       <div className="Header">
         <div id="mainHead">
@@ -53,8 +53,8 @@ class Header extends Component {
           <h1>Kanoo</h1>
           <div id='headerSpace'></div>
           <div className="userButtons">
-            <UserMenu 
-            profile_pic={profileImg}/>
+            <UserMenu
+              profile_pic={profileImg} />
           </div>
         </div>
       </div>
@@ -64,4 +64,4 @@ class Header extends Component {
 
 const mapStateToProps = reduxState => reduxState
 
-export default connect(mapStateToProps, {getUserData})(Header);
+export default connect(mapStateToProps, { getUserData })(Header);
